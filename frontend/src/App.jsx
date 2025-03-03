683b68b96d6e8adefd4397ddc5068ecc0b477ae1
KaraniAbdellah <abdellahkarani@gmail.com>
2025-03-03 15:06:10 +0000

    setUp

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <h1 className='text-red-800 text-6xl flex justify-center font-semibold'>
+      Please Work in your Folder -- check note.txt file
+    </h1>
+  );
+}
+
+export default App
