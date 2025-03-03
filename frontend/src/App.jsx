60842224ee12d9dac873f64873a6ec856fa29280
anassazz <anass.azz2030@gmail.com>
2025-03-03 16:19:21 +0100

    first commit from anass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <h1 className='text-red-800 text-6xl flex justify-center font-semibold'>
-      Please Work in your Folder -- check note.txt file
+      Please Work in your Folder -- check note.txt file from Anass
     </h1>
   );
 }
